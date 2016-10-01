4802e2b60108f6bf925667c6ab1bfc1153a79ab1
Zukhra Amineva <zukhrusha1994@gmail.com>
2016-10-01 13:39:17 +0500

    first commit

---
+++ Quality_control_git.cpp
@@ -0,0 +1,14 @@
+// Quality_control_git.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	cout << "Hello WORLD!" << endl;
+	return 0;
+}
+
